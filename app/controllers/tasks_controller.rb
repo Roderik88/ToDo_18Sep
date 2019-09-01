cbd11547dfedd69963db85dd9d175d78777a1ac5
Rodrigo <rodrigo.lindenmeyer.toledo@gmail.com>
2019-09-01 18:50:16 -0400

    create incomplete details page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,9 @@ class TasksController < ApplicationController
   def index
     @task = Task.all
   end
+
+  def details
+    @tasks = Task.all
+    @usertasks = Usertask.find_by(task: @task, user: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
   resources :usertasks, only: :index
 
-
   root to: 'tasks#index'
+  get 'tasks/details', to: 'tasks#details', as: 'details'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
